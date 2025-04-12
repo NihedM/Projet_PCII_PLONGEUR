34755270ccb70c4c829d5649d52567c8e246c1ba
Nihed <nihed.mansouri@etu-upsaclay.fr>
2025-04-12 14:33:38 +0200

    B/T

---
+++ src/model/ressources/Tresor.java
@@ -0,0 +1,12 @@
+package model.ressources;
+
+import model.objets.Position;
+import model.objets.Ressource;
+
+
+public class Tresor extends Ressource {
+    public Tresor(Position position) {
+        super(position, 10, 30, 15); // Exemple : nom et valeur de la ressource
+    }
+
+}
